eb020b8c00e20fb245919c6a917dcbc94d4ee067
ss4936 <shahsahilj@gmail.com>
2016-12-17 17:40:56 -0500

    data

--- DataScraping/dataFill.py
+++ DataScraping/dataFill.py
@@ -1,5 +1,5 @@
 import psycopg2
-import urllib2
+#import urllib2
 import json
 from watson_developer_cloud import AlchemyLanguageV1
 from bs4 import BeautifulSoup
@@ -29,7 +29,7 @@ def doQuery(conn) :
                 pdic[data[0]]=pid
                 pid+=1
             if(data[4] not in cdic):
-                # cur.execute("INSERT INTO course VALUES (%s, %s, %s, %s, %s, %s)",(cid, data[4],data[5],data[1],data[6],""))
+                cur.execute("INSERT INTO course VALUES (%s, %s, %s, %s, %s, %s)",(cid, data[4],data[5],data[1],data[6],""))
                 cdic[data[4]] = cid
                 cid+=1
             #cur.execute("INSERT INTO building VALUES (%s, %s, %s, %s)",(data[0],data[1],float(data[2]), float(data[3])))
@@ -37,10 +37,10 @@ def doQuery(conn) :
         for line in f:
             data_og = line.split('$')
             data = [i.strip() for i in data_og]
-            print data
+            #print data
             cur.execute("INSERT INTO course_prof2 VALUES (%s, %s, %s, %s, %s, %s)",(cdic[data[4]], pdic[data[0]],data[7],data[8],data[9],data[10]))
     #put_reviews(cur, pdic, cdic)
-
+'''
 def put_reviews(cur, pdic, cdic):
     professors = ["http://culpa.info/professors/2742", "http://culpa.info/professors/4500", "http://culpa.info/professors/2568", "http://culpa.info/professors/13217", "http://culpa.info/professors/1442", "http://culpa.info/professors/13116", "http://culpa.info/professors/375", "http://culpa.info/professors/3366", "http://culpa.info/professors/13076", "http://culpa.info/professors/1724", "http://culpa.info/professors/2941"];
     #professors = ["http://culpa.info/professors/375"];
@@ -101,7 +101,7 @@ def put_reviews(cur, pdic, cdic):
                 #print cid, pid,review#,sentiment_array[i]
         print count
 
-
+'''
 
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 doQuery( myConnection )
