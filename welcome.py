64127429e48539c842e957742f760e19b52543dd
parthrparekh93 <parthrparekh93@gmail.com>
2016-12-16 19:27:07 -0500

    Server initial

--- welcome.py
+++ welcome.py
@@ -14,6 +14,7 @@
 
 import os
 from flask import Flask, jsonify
+from socket import *
 
 app = Flask(__name__)
 
@@ -21,25 +22,6 @@ app = Flask(__name__)
 def Welcome():
     return app.send_static_file('index.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
-
-port = os.getenv('PORT', '5000')
+port = os.getenv('PORT', '8002')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
