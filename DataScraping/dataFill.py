adc2ace41eff4ff3cda6d37c5a073c26d745d96c
ss4936 <shahsahilj@gmail.com>
2016-12-17 18:32:36 -0500

    migrated to elephantsql

--- DataScraping/dataFill.py
+++ DataScraping/dataFill.py
@@ -1,5 +1,5 @@
 import psycopg2
-#import urllib2
+import urllib2
 import json
 from watson_developer_cloud import AlchemyLanguageV1
 from bs4 import BeautifulSoup
@@ -25,11 +25,11 @@ def doQuery(conn) :
             data = [i.strip() for i in data_og]
             #print data
             if(data[0] not in pdic):
-                cur.execute("INSERT INTO professor2 VALUES (%s, %s, %s, %s, %s)",(pid,data[0],data[1],data[2],data[3]))
+                #cur.execute("INSERT INTO professor2 VALUES (%s, %s, %s, %s, %s)",(pid,data[0],data[1],data[2],data[3]))
                 pdic[data[0]]=pid
                 pid+=1
             if(data[4] not in cdic):
-                cur.execute("INSERT INTO course VALUES (%s, %s, %s, %s, %s, %s)",(cid, data[4],data[5],data[1],data[6],""))
+                #cur.execute("INSERT INTO course VALUES (%s, %s, %s, %s, %s, %s)",(cid, data[4],data[5],data[1],data[6],""))
                 cdic[data[4]] = cid
                 cid+=1
             #cur.execute("INSERT INTO building VALUES (%s, %s, %s, %s)",(data[0],data[1],float(data[2]), float(data[3])))
@@ -39,8 +39,8 @@ def doQuery(conn) :
             data = [i.strip() for i in data_og]
             #print data
             cur.execute("INSERT INTO course_prof2 VALUES (%s, %s, %s, %s, %s, %s)",(cdic[data[4]], pdic[data[0]],data[7],data[8],data[9],data[10]))
-    #put_reviews(cur, pdic, cdic)
-'''
+    put_reviews(cur, pdic, cdic)
+
 def put_reviews(cur, pdic, cdic):
     professors = ["http://culpa.info/professors/2742", "http://culpa.info/professors/4500", "http://culpa.info/professors/2568", 
     "http://culpa.info/professors/13217", "http://culpa.info/professors/1442", "http://culpa.info/professors/13116", 
@@ -53,51 +53,51 @@ def put_reviews(cur, pdic, cdic):
     #professors = ["http://culpa.info/professors/375"];
     pids = [1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 15, 16 ,17, 18, 19, 20, 21, 22, 23, 24, 27, 28, 29]
     for j,professor in enumerate(professors):
-    	page = urllib2.urlopen(professor)
-    	soup = BeautifulSoup(page)
+        page = urllib2.urlopen(professor)
+        soup = BeautifulSoup(page)
         pid = pids[j]
 
-    	# print soup.prettify().encode('UTF-8')
-    	# print soup.find('div', class_='professor').find('div', class_='box').find('h1').contents[0]
-    	reviews_array = list()
-    	reviewToCourse = dict()
-    	professor_info = soup.find('div', class_='professor').find('div', class_='box')
-    	professor_name = professor_info.find('h1').contents[0].strip()
-    	#dept_name = professor_info.contents[5].find('a').string.strip()
-    	course_name = professor_info.contents[7].find('a').string.strip()
-    	reviews = soup.find_all('div', class_='review_content')
+        # print soup.prettify().encode('UTF-8')
+        # print soup.find('div', class_='professor').find('div', class_='box').find('h1').contents[0]
+        reviews_array = list()
+        reviewToCourse = dict()
+        professor_info = soup.find('div', class_='professor').find('div', class_='box')
+        professor_name = professor_info.find('h1').contents[0].strip()
+        #dept_name = professor_info.contents[5].find('a').string.strip()
+        course_name = professor_info.contents[7].find('a').string.strip()
+        reviews = soup.find_all('div', class_='review_content')
 
-    	reviews2 = soup.find_all('div', class_='meta')
+        reviews2 = soup.find_all('div', class_='meta')
 
-    	for review in reviews:
-    		temp = ""
-    		statements = review.contents
-    		for statement in statements:
-    			if statement.string is not None:
-    				temp += statement.string
-    		reviews_array.append(temp)
+        for review in reviews:
+            temp = ""
+            statements = review.contents
+            for statement in statements:
+                if statement.string is not None:
+                    temp += statement.string
+            reviews_array.append(temp)
 
-      	for i, review in enumerate(reviews2):
-    		try:
-    			reviewToCourse[reviews_array[i]] = review.contents[3].find('a').find('span',class_='course_name').string.strip()
-    		except:
-    			reviewToCourse[reviews_array[i]] = course_name
+        for i, review in enumerate(reviews2):
+            try:
+                reviewToCourse[reviews_array[i]] = review.contents[3].find('a').find('span',class_='course_name').string.strip()
+            except:
+                reviewToCourse[reviews_array[i]] = course_name
 
-    	print professor_name
-    	#print dept_name
-    	# print course_name
-    	# print reviews_array
-    	# print len(reviews_array)
+        print professor_name
+        #print dept_name
+        # print course_name
+        # print reviews_array
+        # print len(reviews_array)
         sentiment_array = list()
         emotion_array = []
         language_array = []
         social_array = []
 
-    	for review in reviews_array:
+        for review in reviews_array:
             emotion_array.append(tone_analyzer.get_emotions(review))
             language_array.append(tone_analyzer.get_language(review))
             social_array.append(tone_analyzer.get_social(review))
-        	sentiment_array.append(json.dumps(alchemy_language.sentiment(text=review)["docSentiment"]["type"]))
+            sentiment_array.append(json.dumps(alchemy_language.sentiment(text=review)["docSentiment"]["type"]))
         count = 0
         for i, review in enumerate(reviewToCourse):
             print reviewToCourse[review]
@@ -108,7 +108,7 @@ def put_reviews(cur, pdic, cdic):
                 #print cid, pid,review#,sentiment_array[i]
         print count
 
-'''
+
 
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 doQuery( myConnection )
